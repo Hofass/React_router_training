767ac14223979e87e018838ef6fed633f5358ca5
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-07-05 20:29:59 +0200

    Adding links for dynamic links

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+import classes from "../components/MainNavigation.module.css"
+
 function ProductPage(){
 return <>
 <h1>Product Page</h1>
-<ul>
-  <li>Product 1</li>
-  <li>Product 2</li>
-  <li>Product 3</li>
+<ul className={classes.list}>
+  <li><Link to="/products/product-1">Product 1</Link></li>
+  <li><Link to="/products/product-2">Product 2</Link></li>
+  <li><Link to="/products/product-3">Product 3</Link></li>
+
 </ul>
 
 </>
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -2,13 +2,9 @@ import { useParams } from "react-router-dom";
 
 function ProductDetails() {
   const params = useParams();
-  
-
-
 	return <>
-  <h1>Product Details!</h1>;
+  <h1>Product Details!</h1>
   <p>{params.productId}</p>
-  
   </>
   
 };
